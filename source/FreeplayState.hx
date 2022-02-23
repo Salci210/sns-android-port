27928907186e0eab80ba254e2ce6dff170cf65b2
El pepe <85134096+Salci210@users.noreply.github.com>
2022-02-22 22:16:20 -0600

    update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -18,28 +18,7 @@ import flixel.system.FlxSound;
 import openfl.utils.Assets as OpenFlAssets;
 import WeekData;
 
-	addVirtualPad(FULL, A_B);
-
-	//if you want it to have a camera
-	addPadCamera()
-
-	//in states, those needs to be added before super.create();
-	//in substates, in fuction new at the last line add those
-
-	//on Playstate.hx after all
-	//obj.camera = ...
-	//add
-	addAndroidControls();
-
-	//to make the controls visible the code is
-	#if android
-	androidc.visible = true;
-	#end
-
-	//to make the controls invisible the cose is
-	#if android
-	androidc.visible = false;
-	#end
+	
 
 using StringTools;
 
@@ -192,6 +171,28 @@ class FreeplayState extends MusicBeatState
 		add(text);
 		super.create();
 	}
+	addVirtualPad(FULL, A_B);
+
+	//if you want it to have a camera
+	addPadCamera()
+
+	//in states, those needs to be added before super.create();
+	//in substates, in fuction new at the last line add those
+
+	//on Playstate.hx after all
+	//obj.camera = ...
+	//add
+	addAndroidControls();
+
+	//to make the controls visible the code is
+	#if android
+	androidc.visible = true;
+	#end
+
+	//to make the controls invisible the cose is
+	#if android
+	androidc.visible = false;
+	#end
 
 	override function closeSubState() {
 		changeSelection();
