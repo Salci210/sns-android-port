e79650c385f29972f2be8602e67cb799433f3e64
El pepe <85134096+Salci210@users.noreply.github.com>
2022-02-22 20:31:23 -0600

    update OptionsState.hx

--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -25,6 +25,29 @@ import flixel.input.keyboard.FlxKey;
 import flixel.graphics.FlxGraphic;
 import Controls;
 
+	addVirtualPad(FULL, A_B);
+
+	//if you want it to have a camera
+	addPadCamera()
+
+	//in states, those needs to be added before super.create();
+	//in substates, in fuction new at the last line add those
+
+	//on Playstate.hx after all
+	//obj.camera = ...
+	//add
+	addAndroidControls();
+
+	//to make the controls visible the code is
+	#if android
+	androidc.visible = true;
+	#end
+
+	//to make the controls invisible the cose is
+	#if android
+	androidc.visible = false;
+	#end
+
 using StringTools;
 
 // TO DO: Redo the menu creation system for not being as dumb
