852e239fc26d505c3664e92f2ec24b2527267ccf
El pepe <85134096+Salci210@users.noreply.github.com>
2022-02-22 22:38:07 -0600

    update MusicBeatSubstate.hx

--- source/MusicBeatSubstate.hx
+++ source/MusicBeatSubstate.hx
@@ -13,28 +13,7 @@ class MusicBeatSubstate extends FlxSubState
 	public function new()
 	{
 		super();
-	addVirtualPad(FULL, A_B);
-
-	//if you want it to have a camera
-	addPadCamera()
-
-	//in states, those needs to be added before super.create();
-	//in substates, in fuction new at the last line add those
-
-	//on Playstate.hx after all
-	//obj.camera = ...
-	//add
-	addAndroidControls();
-
-	//to make the controls visible the code is
-	#if android
-	androidc.visible = true;
-	#end
-
-	//to make the controls invisible the cose is
-	#if android
-	androidc.visible = false;
-	#end
+	
 	}
 
 	private var lastBeat:Float = 0;
