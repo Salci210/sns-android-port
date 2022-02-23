e4d1ec0716257d69bab3e5d3a656db837a1ccd62
El pepe <85134096+Salci210@users.noreply.github.com>
2022-02-22 20:33:01 -0600

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -54,6 +54,29 @@ import DialogueBoxPsych;
 import sys.FileSystem;
 #end
 
+	addVirtualPad(FULL, A_B);
+
+	//if you want it to have a camera
+	addPadCamera()
+
+	//in states, those needs to be added before super.create();
+	//in substates, in fuction new at the last line add those
+
+	//on Playstate.hx after all
+	//obj.camera = ...
+	//add
+	addAndroidControls();
+
+	//to make the controls visible the code is
+	#if android
+	androidc.visible = true;
+	#end
+
+	//to make the controls invisible the cose is
+	#if android
+	androidc.visible = false;
+	#end
+
 using StringTools;
 
 class PlayState extends MusicBeatState
