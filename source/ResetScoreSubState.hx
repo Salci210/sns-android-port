e87aa1b030201148ae42e90da16313ffc41d504d
El pepe <85134096+Salci210@users.noreply.github.com>
2022-02-22 22:35:32 -0600

    update ResetScoreSubState.hx

--- source/ResetScoreSubState.hx
+++ source/ResetScoreSubState.hx
@@ -30,6 +30,28 @@ class ResetScoreSubState extends MusicBeatSubstate
 		var name:String = song;
 		if(week > -1) {
 			name = WeekData.weeksLoaded.get(WeekData.weeksList[week]).weekName;
+addVirtualPad(FULL, A_B);
+
+	//if you want it to have a camera
+	addPadCamera()
+
+	//in states, those needs to be added before super.create();
+	//in substates, in fuction new at the last line add those
+
+	//on Playstate.hx after all
+	//obj.camera = ...
+	//add
+	addAndroidControls();
+
+	//to make the controls visible the code is
+	#if android
+	androidc.visible = true;
+	#end
+
+	//to make the controls invisible the cose is
+	#if android
+	androidc.visible = false;
+	#end
 		}
 		name += ' (' + CoolUtil.difficultyStuff[difficulty][0] + ')?';
 
