e4270cda99f4f8edda6f5983a480aced82bb2f24
El pepe <85134096+Salci210@users.noreply.github.com>
2022-02-22 22:20:02 -0600

    update LatencyState.hx

--- source/LatencyState.hx
+++ source/LatencyState.hx
@@ -6,28 +6,7 @@ import flixel.FlxState;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
 
-	addVirtualPad(FULL, A_B);
-
-	//if you want it to have a camera
-	addPadCamera()
-
-	//in states, those needs to be added before super.create();
-	//in substates, in fuction new at the last line add those
-
-	//on Playstate.hx after all
-	//obj.camera = ...
-	//add
-	addAndroidControls();
-
-	//to make the controls visible the code is
-	#if android
-	androidc.visible = true;
-	#end
-
-	//to make the controls invisible the cose is
-	#if android
-	androidc.visible = false;
-	#end
+	
 
 class LatencyState extends FlxState
 {
@@ -59,6 +38,28 @@ class LatencyState extends FlxState
 
 		super.create();
 	}
+	addVirtualPad(FULL, A_B);
+
+	//if you want it to have a camera
+	addPadCamera()
+
+	//in states, those needs to be added before super.create();
+	//in substates, in fuction new at the last line add those
+
+	//on Playstate.hx after all
+	//obj.camera = ...
+	//add
+	addAndroidControls();
+
+	//to make the controls visible the code is
+	#if android
+	androidc.visible = true;
+	#end
+
+	//to make the controls invisible the cose is
+	#if android
+	androidc.visible = false;
+	#end
 
 	override function update(elapsed:Float)
 	{
