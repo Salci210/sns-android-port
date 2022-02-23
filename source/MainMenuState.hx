04567e71cd7e1001b6fb4f7b2ba9bbb678186516
El pepe <85134096+Salci210@users.noreply.github.com>
2022-02-22 22:22:04 -0600

    update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -20,28 +20,7 @@ import lime.app.Application;
 import Achievements;
 import editors.MasterEditorMenu;
 
-	addVirtualPad(FULL, A_B);
-
-	//if you want it to have a camera
-	addPadCamera()
-
-	//in states, those needs to be added before super.create();
-	//in substates, in fuction new at the last line add those
-
-	//on Playstate.hx after all
-	//obj.camera = ...
-	//add
-	addAndroidControls();
-
-	//to make the controls visible the code is
-	#if android
-	androidc.visible = true;
-	#end
-
-	//to make the controls invisible the cose is
-	#if android
-	androidc.visible = false;
-	#end
+	
 
 using StringTools;
 
@@ -154,6 +133,29 @@ class MainMenuState extends MusicBeatState
 
 		super.create();
 	}
+
+	addVirtualPad(FULL, A_B);
+
+	//if you want it to have a camera
+	addPadCamera()
+
+	//in states, those needs to be added before super.create();
+	//in substates, in fuction new at the last line add those
+
+	//on Playstate.hx after all
+	//obj.camera = ...
+	//add
+	addAndroidControls();
+
+	//to make the controls visible the code is
+	#if android
+	androidc.visible = true;
+	#end
+
+	//to make the controls invisible the cose is
+	#if android
+	androidc.visible = false;
+	#end
 
 	#if ACHIEVEMENTS_ALLOWED
 	// Unlocks "Freaky on a Friday Night" achievement
